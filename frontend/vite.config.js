f99f36fef3b54f2a78bafd54b740b9cb24797a84
yclin30 <2729934359@qq.com>
2026-05-05 19:26:48 +0800

    更新ci

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -35,6 +35,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.js',
+    reporters: ['default', 'junit'],
+    outputFile: {
+      junit: './junit.xml'
+    },
     coverage: {
       provider: 'v8',
       reporter: ['text', 'lcov', 'html'],
